a85517a774a4345a924f0089894e3819120822b5
SimranJha24 <137113520+SimranJha24@users.noreply.github.com>
2025-07-31 13:47:58 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,14 +14,14 @@ export default defineConfig(async () => {
 
   return {
     base: '/Newsoftheweek/',
+    plugins,
     build: {
       outDir: path.resolve(__dirname, 'dist'),
       emptyOutDir: true,
     },
-    plugins,
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, 'client', 'src'),
+        '@': path.resolve(__dirname, 'src'),
         '@shared': path.resolve(__dirname, 'shared'),
         '@assets': path.resolve(__dirname, 'attached_assets'),
       },
